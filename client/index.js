46906e99a90c78628bf09b170fb70e11c1fa8ce6
FelixYeung2002 <felix.yeung.minecraft@gmail.com>
2020-07-30 19:26:16 +0800

    Changed port to 8000

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 8000;
 const publicWebFolder = __dirname + "/web";
 
 app.use("/", express.static(publicWebFolder));
