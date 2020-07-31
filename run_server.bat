d9e396c2e3ee6cbc32752a78e19031fa8f8adbfe
FelixYeung2002 <felix.yeung.minecraft@gmail.com>
2020-08-01 04:19:27 +0800

    Server windows script

--- run_server.bat
+++ run_server.bat
@@ -1,5 +1,7 @@
 @echo off
 cd server
-fpc @fpc.cfg lib/main.pas -FuC:\FPC\3.0.4\units\i386-win32\*;
-.\lib\main.exe;
+:start
+fpc @fpc.cfg lib/main.pas -FuC:\FPC\3.0.4\units\i386-win32\*
+.\lib\main.exe
+goto start
 cd ..
