f84233db3a1c609650b307a89d66f902bd0eb57d
Felix Yeung <felix.yeung2002@gmail.com>
2020-08-02 12:55:50 +0800

    Fixed server fatal errors

--- server/lib/misc/feli_stack_tracer.pas
+++ server/lib/misc/feli_stack_tracer.pas
@@ -43,12 +43,12 @@ end;
 class procedure FeliStackTrace.trace(kind, name: ansiString); static;
 var
     tempString, spaces: ansiString;
-    i, depth: int64;
+    i, depth: integer;
     
 begin
     spaces := '';
     tempString := FeliFileAPI.get(stackTraceDepthPath);
-    depth := StrToInt64(trim(tempString));
+    depth := StrToInt(trim(tempString));
     case kind of
         'begin': depth := depth + 1;
         'end': depth := depth - 1;
@@ -68,12 +68,12 @@ end;
 class procedure FeliStackTrace.out(load: String); static;
 var
     tempString, spaces: ansiString;
-    i, depth: int64;
+    i, depth: integer;
     
 begin
     spaces := '';
     tempString := FeliFileAPI.get(stackTraceDepthPath);
-    depth := StrToInt64(trim(tempString));
+    depth := StrToInt(trim(tempString));
     if ((depth - 4) <> 0) then for i := 0 to (depth - 4) do spaces := spaces + '| ';
     if (tempString = 'end') then tempString := ' end ';
     spaces := simpleSpaceReplaceText(spaces);
