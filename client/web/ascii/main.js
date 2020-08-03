c3102db5f76d9fbc9c001620f7718b4af41cd4db
FelixYeung2002 <felix.yeung.minecraft@gmail.com>
2020-08-03 23:13:44 +0800

    Ascii art iamge overlay

--- client/web/ascii/main.js
+++ client/web/ascii/main.js
@@ -2,6 +2,8 @@ let uploadForm = document.querySelector("#uploadForm");
 let inputFile = document.querySelector("#inputFile");
 let ascii = document.querySelector("#ascii");
 let fileName = document.querySelector("#fileName");
+let imagePreview = document.querySelector("#imagePreview");
+
 
 inputFile.addEventListener("change", (e) => {
     fileName.innerHTML = `${inputFile.files[0].name}`;
@@ -12,12 +14,23 @@ uploadForm.addEventListener("submit", async (e) => {
     // var size = 100;
     const endpoint =
         "http://dynamic.felixyeung2002.com:8081/api/ascii/?size=50&size=50";
-        // `http://192.168.1.3:8081/api/ascii/?size=${size}&size=${size}`;
+    // `http://192.168.1.3:8081/api/ascii/?size=${size}&size=${size}`;
     const formData = new FormData();
 
     formData.append("image", inputFile.files[0]);
+    let file = inputFile.files[0];
+    if (file) {
+        const reader = new FileReader();
 
-    ascii.innerHTML = 'Loading...'
+        reader.addEventListener('load', () => {
+            imagePreview.src= (`${reader.result}`)
+        }, false)
+    
+        reader.readAsDataURL(file);
+    }
+
+
+    ascii.innerHTML = "Loading...";
 
     scrollTo({
         top: document.body.clientHeight,
@@ -35,7 +48,7 @@ uploadForm.addEventListener("submit", async (e) => {
 
     if (json && json["message"]) {
         ascii.innerHTML = json["message"];
+        imagePreview.width = ascii.clientWidth;
+        imagePreview.height = ascii.clientHeight;
     }
-
-    
 });
